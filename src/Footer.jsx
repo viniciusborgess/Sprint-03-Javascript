9202bfde1f7fe3f583c7d3a72fbd22e1d97f96b6
Vinicius <viniciussobreiraborges@hotmail.com>
2023-09-15 14:33:30 -0300

    Instalado React Bootstrap

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,8 +6,14 @@ function Footer() {
 
   return (
     <>
-    <footer>
-    </footer>
+    <footer className="text-center rodape col-sm-12 col-md-12 col-lg-12">
+  <figure>
+    <img src="./images/footer-logo.png" alt="Imagem 1" className="img-fluid efeito-fotos" />
+  </figure>
+
+  <p className="text-info-emphasis">A Smart Light oferece produtos e serviços de qualidade com preços e condições diferenciadas para você migrar para energia solar.</p>
+</footer>
+
     </>
   )
 }
